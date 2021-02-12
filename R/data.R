d59b4ae3e879914737bec73fadb1158e86c1901c
Vincent Runge <vincent.runge@univ-evry.fr>
2021-02-12 18:54:20 +0100

    add data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' gfpop.data
+#'
+#' @description Load all the gfpop datasets
+#' @examples
+#' gfpop.data()
+gfpop.data <- function()
+{
+  data("ECG")
+  data("neuroSpike")
+  data("profile614chr2")
+  data("Mono27ac")
+}
